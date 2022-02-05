9922ab3bf086fd37919cbdcfa587400fc811ab6f
Vignesh-PS <selvavignesh128@gmail.com>
2022-02-05 12:46:04 +0530

    Fixed merge conflicts, media files remain not uploaded.

--- hof.js
+++ hof.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var _lodash = require("https://cdn.skypack.dev/lodash@4.17.20");
 class Carousel {
   static getDest(element) {
@@ -75,82 +74,4 @@ function isOdd(value) {
   return value % 2 === 0;
 }
 
-=======
-var _lodash = require("https://cdn.skypack.dev/lodash@4.17.20");
-class Carousel {
-  static getDest(element) {
-    if (!(element instanceof Element)) return;
-    if (element === document.documentElement) return;
-    if (element.classList.contains("destination")) return element;
-    return Carousel.getDest(element.parentElement);
-  }
-
-  constructor(element) {
-    this.wrapperElement = element;
-    this.dests = [...element.querySelectorAll(".destination")];
-    this.active = null;
-    this.update();
-    this.wrapperElement.classList.remove("loading");
-
-    this.wrapperElement.addEventListener("mouseover", (event) => {
-      const dest = Carousel.getDest(event.target);
-      if (typeof dest === "undefined") return;
-      if (dest === this.dests[this.active]) return;
-      this.activate(dest);
-    });
-
-    this.wrapperElement.addEventListener(
-      "mouseleave",
-      _lodash((event) => {
-        console.log("mouseleave happening");
-        if (event.target === this.wrapperElement) this.deactivate();
-      }, 500)
-    );
-  }
-
-  getIndex(dest) {
-    if (!this.dests.includes(dest)) return;
-    let i = 0;
-    for (let currentDest of this.dests) {
-      if (dest === currentDest) {
-        return i;
-      }
-      i++;
-    }
-  }
-
-  activate(dest) {
-    this.active = this.getIndex(dest) ?? null;
-    this.update();
-  }
-
-  deactivate() {
-    this.active = null;
-    this.update();
-  }
-
-  update() {
-    this.dests.forEach((dest, index) => {
-      dest.className = "destination";
-
-      if (index === this.active) {
-        dest.classList.add("unfolded", isOdd(index) ? "back" : "front");
-      } else {
-        dest.classList.add("folded");
-
-        if (this.active === null || index < this.active) {
-          dest.classList.add(isOdd(index) ? "right" : "left");
-        } else if (index > this.active) {
-          dest.classList.add(isOdd(index) ? "left" : "right");
-        }
-      }
-    });
-  }
-}
-
-function isOdd(value) {
-  return value % 2 === 0;
-}
-
->>>>>>> master
 const carousel = new Carousel(document.querySelector(".wrapper"));
